592eaa0d729443ec086d25dda83588162510eb22
souravkumar <souravkumar31cse@gmail.com>
2023-11-27 16:53:24 +0530

    login php page

---
+++ login.php
@@ -0,0 +1,107 @@
+<?php
+
+$sname= "localhost";
+
+$unmae= "root";
+
+$password = "";
+
+$db_name = "test_db";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+
+}
+
+This demo has used an if statement to check whether the code is working or not. A suitable message is printed, depending on if the database was found. 
+
+Now, execute the below SQL query to create the user table within your MySQL database.
+
+<?php 
+
+session_start(); 
+
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['uname']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['user_name'] = $row['user_name'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: home.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
